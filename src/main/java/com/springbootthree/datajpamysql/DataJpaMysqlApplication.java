6287533680199cff8676cbb6e2bbade5aadf0b18
prashanthsriram145 <prashanthsriram145@gmail.com>
2021-08-01 21:40:53 +0530

    initial commit

---
+++ src/main/java/com/springbootthree/datajpamysql/DataJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springbootthree.datajpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootthree/datajpamysql/DataJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootthree.datajpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
